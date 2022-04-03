2bd74674fb83d231af7339367ba72955d15cd0e3
mike <mike@hiko.co.nz>
2022-04-03 17:42:44 +1200

    kkk

--- pages/painting/index.js
+++ pages/painting/index.js
@@ -3,7 +3,6 @@ import { attributes as paintAtt } from "../../content/paintings.md";
 import { Slide } from "react-slideshow-image";
 import { useRouter } from "next/router";
 
-import "react-slideshow-image/dist/styles.css";
 export default function Paintings() {
   const router = useRouter();
   const { category } = router.query;
@@ -13,8 +12,11 @@ export default function Paintings() {
       <Slide>
         {slideImages.map(({ image, title, price, size, medium, categories }, index) => (
           <div className="each-slide" key={index}>
-            <div style={{ backgroundImage: `url(${image})` }}>
+            <p>
               <span>{title}</span>
+            </p>
+            <div>
+              <img src={image} alt={title} />
             </div>
           </div>
         ))}
