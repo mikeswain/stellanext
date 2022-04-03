a554cc2f676cbc4e1b3b9999c98dd4376c715903
mike <mike@hiko.co.nz>
2022-04-03 16:10:50 +1200

    css

--- pages/painting/index.js
+++ pages/painting/index.js
@@ -1,19 +1,24 @@
-import { attributes } from "../../content/categories.md";
-import { paintings } from "../../content/paintings.md";
+import { attributes as catAtt } from "../../content/categories.md";
+import { attributes as paintAtt } from "../../content/paintings.md";
 import { Slide } from "react-slideshow-image";
+import { useRouter } from "next/router";
+
+import "react-slideshow-image/dist/styles.css";
 export default function Paintings() {
   const router = useRouter();
   const { category } = router.query;
-  const slideImages = attributes.paintings.map({ur})
-  <div className="slide-container">
-    <Slide>
-      {slideImages.map((slideImage, index) => (
-        <div className="each-slide" key={index}>
-          <div style={{ backgroundImage: `url(${slideImage.url})` }}>
-            <span>{slideImage.caption}</span>
+  const slideImages = paintAtt.paintings.filter(({ categories }) => !category || catAtt.categories.contains(category));
+  return (
+    <div className="slide-container">
+      <Slide>
+        {slideImages.map(({ image, title, price, size, medium, categories }, index) => (
+          <div className="each-slide" key={index}>
+            <div style={{ backgroundImage: `url(${image})` }}>
+              <span>{title}</span>
+            </div>
           </div>
-        </div>
-      ))}
-    </Slide>
-  </div>;
+        ))}
+      </Slide>
+    </div>
+  );
 }
