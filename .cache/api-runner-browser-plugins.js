f0d2b54a3a4a840ea11dcd190cdfa5b054c97075
mike <mike@hiko.co.nz>
2022-04-18 11:51:16 +1200

    fixed untracked files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
-      options: {"plugins":[],"modulePath":"/home/mike/stellanext/stellanext/src/cms/cms.js"},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-use-query-params/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- .cache/async-requires.js
+++
@@ -1,7 +0,0 @@
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-markdown-remark-fields-slug-js": () => import("./../../../src/pages/{MarkdownRemark.fields__slug}.js" /* webpackChunkName: "component---src-pages-markdown-remark-fields-slug-js" */),
-  "component---src-pages-paintings-index-js": () => import("./../../../src/pages/paintings/index.js" /* webpackChunkName: "component---src-pages-paintings-index-js" */)
-}
-
